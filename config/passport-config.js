cffaa2d1f11f554ca9b46e036faef297a3f0da9c
DiDex <wc34fun@mail.ru>
2018-10-26 17:03:45 +0800

    initial commit

---
+++ config/passport-config.js
@@ -0,0 +1,47 @@
+const passport = require('passport')
+const passportJWT = require('passport-jwt')
+const LocalStrategy = require('passport-local').Strategy
+const mongoose = require('mongoose')
+const User = mongoose.model('login')
+const config = require('./config')
+const ExtractJwt = passportJWT.ExtractJwt
+const Strategy = passportJWT.Strategy
+
+const params = {
+  secretOrKey: config.secret,
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+}
+
+passport.serializeUser((user, done) => done(null, user.id))
+
+passport.deserializeUser((id, done) => User.findById(id, (err, user) => done(err, user)))
+
+passport.use(
+  'loginUsers',
+  new LocalStrategy((login, password, done) => {
+    User.findOne({login})
+      .then(user => {
+        if (user.validPassword(password)) {
+          return done(null, user)
+        } else {
+          return done(null, false)
+        }
+      })
+      .catch(err => {
+        console.log(err)
+        done(err)
+      })
+  }),
+)
+
+var strategy = new Strategy(params, ({id}, done) => {
+  const User = mongoose.model('login')
+  User.find({id})
+    .then(({id}) => (id ? done(null, {id}) : done(new Error('User not found'), null)))
+    .catch(err => {
+      console.log(err)
+      done(err)
+    })
+})
+
+passport.use(strategy)
